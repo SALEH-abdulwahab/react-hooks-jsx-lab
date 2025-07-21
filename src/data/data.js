ed7df4ad609356676fd8087cb3dbfc4f52083cb7
SALEH-abdulwahab <saleh200228@gmail.com>
2025-07-21 22:18:35 +0300

    cm

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "saleh";
+const city = "nairobi";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://www.google.com/imgres?q=dog&imgurl=https%3A%2F%2Fwww.dogstrust.org.uk%2Fimages%2F800x600%2Fassets%2F2025-03%2Ftoffee%25202.jpg&imgrefurl=https%3A%2F%2Fwww.dogstrust.org.uk%2Fdog-advice%2Fgetting-dog%2Fchoosing-dog%2Funderdogs&docid=2rgM9iPDp2RT6M&tbnid=c0Zfv9UDr8FXOM&vet=12ahUKEwjn3r_E1M6OAxVaSfEDHf36EvQQM3oECCYQAA..i&w=800&h=600&hcb=2&ved=2ahUKEwjn3r_E1M6OAxVaSfEDHf36EvQQM3oECCYQAA";
 
 export { name, city, image };
